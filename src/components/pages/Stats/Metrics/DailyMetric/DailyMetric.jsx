7bf66f282fae475998e24e85914c2aef4f43fe33
Matías Morales <programacionfrida@gmail.com>
2024-07-12 16:46:30 -0300

    style metrics OK

--- src/components/pages/Stats/Metrics/DailyMetric/DailyMetric.jsx
+++ src/components/pages/Stats/Metrics/DailyMetric/DailyMetric.jsx
@@ -6,15 +6,19 @@ import React from 'react';
 const DailyMetric = () => {
 
     const salesBalance = useSelector(state => state.sales.salesBalance);
+    const totalRevenue = salesBalance.daily?.totalRevenue ?? 0;  //Si el valor obtenido es null o undefined mostramos '0'.
+    // const totalRevenue = ~~salesBalance.daily?.totalRevenue; //Con el operador de doble tilde "~~" obtenemos el número entero.
 
-    const totalRevenue = ~~salesBalance.daily?.totalRevenue;
-
+    let displayRevenue = totalRevenue;
     let suffix = '';
     if (totalRevenue >= 1000 && totalRevenue < 1000000) {
+        displayRevenue = Math.floor(totalRevenue / 1000);
+        console.log(displayRevenue);
         suffix = 'k';
     } else if (totalRevenue >= 1000000) {
+        displayRevenue = Math.floor(totalRevenue / 1000000);
         suffix = 'M';
-    };
+    }
 
     return(
         <div>
@@ -30,12 +34,12 @@ const DailyMetric = () => {
                     </div>
                     <div className={style.labels}>
                         <p>💲</p>
-                        <div className={style.numberCard}>{totalRevenue.toLocaleString()}{suffix}</div>
+                        <div className={style.numberCard}>{displayRevenue.toLocaleString()}{suffix}</div>
                         <span className={style.cardName}>ganancias</span>
                     </div>
                 </div>
                 <div className={style.textBalance}>
-                    <div>3% más que ayer</div>
+                    <div>+3% que ayer</div>
                 </div>
             </div>
         </div>
--- src/components/pages/Stats/Metrics/MonthlyMetric/MonthlyMetric.jsx
+++ src/components/pages/Stats/Metrics/MonthlyMetric/MonthlyMetric.jsx
@@ -6,13 +6,16 @@ import React from 'react';
 const MonthlyMetric = () => {
 
     const salesBalance = useSelector(state => state.sales.salesBalance);
-    
-    const totalRevenue = ~~salesBalance.monthly?.totalRevenue;
+    const totalRevenue = salesBalance.monthly?.totalRevenue ?? 0;
 
+    let displayRevenue = totalRevenue;
     let suffix = '';
     if (totalRevenue >= 1000 && totalRevenue < 1000000) {
+        displayRevenue = Math.floor(totalRevenue / 1000);
+        console.log(displayRevenue);
         suffix = 'k';
     } else if (totalRevenue >= 1000000) {
+        displayRevenue = Math.floor(totalRevenue / 1000000);
         suffix = 'M';
     };
 
@@ -30,12 +33,12 @@ const MonthlyMetric = () => {
                     </div>
                     <div className={style.labels}>
                         <p>💲</p>
-                        <div className={style.numberCard}>{totalRevenue.toLocaleString()}{suffix}</div>
+                        <div className={style.numberCard}>{displayRevenue.toLocaleString()}{suffix}</div>
                         <span className={style.cardName}>ganancias</span>
                     </div>
                 </div>
                 <div className={style.textBalance}>
-                    <div>3% menos que el mes pasado</div>
+                    <div>-3% que el mes pasado</div>
                 </div>
             </div>
         </div>
