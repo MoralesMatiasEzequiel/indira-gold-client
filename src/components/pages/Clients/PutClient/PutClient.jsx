704f03113a790e78b940fe7d68898d2485677ede
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-06-25 12:17:49 -0300

    edit addresses at edit client - navigation hot fix

--- src/components/pages/Clients/PutClient/PutClient.jsx
+++ src/components/pages/Clients/PutClient/PutClient.jsx
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { getClients, getClientById, putClient } from '../../../../redux/clientActions';
+import x from "./img/x.png";
+import edit from "../../../../assets/img/edit.png";
 
 const PutClient = ({ onClientAdded = () => {}}) => {
-
     const { id } = useParams();
     const dispatch = useDispatch();
     const navigate = useNavigate();
-
     const clientDetail = useSelector(state => state.clients.clientDetail);
 
     useEffect(() => {
@@ -17,6 +17,7 @@ const PutClient = ({ onClientAdded = () => {}}) => {
     }, [dispatch, id]);
 
     const [editClient, setEditClient] = useState({});  
+    const [editingAddressIndex, setEditingAddressIndex] = useState(null); // Para controlar qué dirección se está editando
 
     useEffect(() => {    
         if (clientDetail && clientDetail._id === id) {        
@@ -27,16 +28,84 @@ const PutClient = ({ onClientAdded = () => {}}) => {
                 lastname: clientDetail.lastname,
                 email: clientDetail.email,
                 phone: clientDetail.phone,
+                addresses: clientDetail.addresses || [],
                 active: clientDetail.active
             };
             setEditClient(updatedEditClient);
         }
     }, [dispatch, id, clientDetail]);
 
+    //-----------Address-----------//
+    const initalAddressState = {
+        name: "",
+        street: "",
+        number: "",
+        between: "",
+        floor: "",
+        apartment: "",
+        city: "",
+        province: "",
+        postalCode: "",
+        reference: ""
+    };
+
+    const [address, setAddress] = useState(initalAddressState);
+
+    const handleAddressChange = (event) => {
+        const { name, value } = event.target;
+        setAddress(prev => ({ ...prev, [name]: value }));
+    };
+
+    const addAddress = () => {
+        if (editingAddressIndex !== null) {
+            // Si estamos editando una dirección existente
+            const updatedAddresses = [...editClient.addresses];
+            updatedAddresses[editingAddressIndex] = address;
+            
+            setEditClient(prevClient => ({
+                ...prevClient,
+                addresses: updatedAddresses
+            }));
+            
+            setEditingAddressIndex(null);
+        } else {
+            // Si estamos añadiendo una nueva dirección
+            setEditClient(prevClient => ({
+                ...prevClient,
+                addresses: [...(prevClient.addresses || []), address]
+            }));
+        }
+
+        setAddress(initalAddressState);
+    };
+    
+    const removeAddress = (index) => {
+        setEditClient(prevClient => ({
+            ...prevClient,
+            addresses: prevClient.addresses.filter((_, i) => i !== index)
+        }));
+        
+        // Si estábamos editando la dirección que se eliminó, limpiamos el estado
+        if (editingAddressIndex === index) {
+            setEditingAddressIndex(null);
+            setAddress(initalAddressState);
+        }
+    };
+
+    const editExistingAddress = (index) => {
+        const addressToEdit = editClient.addresses[index];
+        setAddress(addressToEdit);
+        setEditingAddressIndex(index);
+    };
+
+    const cancelEdit = () => {
+        setEditingAddressIndex(null);
+        setAddress(initalAddressState);
+    };
+
     //-----------CHANGE-----------//
     const handleChange = (event) => {
         const { name, value } = event.target;
-
         setEditClient((prevClient) => ({
             ...prevClient,
             [name]: value
@@ -48,14 +117,9 @@ const PutClient = ({ onClientAdded = () => {}}) => {
         
         try {
             const response = await dispatch(putClient(editClient));
-            
-            // Espera a que el cliente se actualice antes de redirigir
             await dispatch(getClientById(id));
-            
             onClientAdded(response);
             dispatch(getClients());
-            
-            // Redirige después de actualizar el cliente
             navigate(`/main_window/clients/${id}`);
         } catch (error) {
             console.error('Error updating client:', error);
@@ -107,8 +171,6 @@ const PutClient = ({ onClientAdded = () => {}}) => {
                                     required 
                                 />
                             </div>
-                        </div>
-                        <div className={style.column}>
                             <div className={style.labelInput}>
                                 <label htmlFor="email">Email</label>
                                 <input 
@@ -131,6 +193,98 @@ const PutClient = ({ onClientAdded = () => {}}) => {
                                     required 
                                 />
                             </div>
+                            <div className={style.labelInput}><label>Direcciones</label></div>
+                            <div className={style.newAddress}>
+                                {editClient.addresses?.length > 0 ? 
+                                    <ul>
+                                        {editClient.addresses.map((address, index) => (
+                                            <li key={index}>
+                                                {address.name && address.name}
+                                                <div>
+                                                    <button type="button" onClick={() => editExistingAddress(index)}>
+                                                        <img src={edit} alt="Editar" />
+                                                    </button>
+                                                    <button type="button" onClick={() => removeAddress(index)}>
+                                                        <img src={x} alt="Eliminar" />
+                                                    </button>
+                                                </div>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                :
+                                    <label style={{fontStyle: "italic"}}>Aún no se han añadido direcciones.</label>}
+                            </div>
+                        </div>
+                        <div className={style.column}>
+                            <div className={style.newAddress}>
+                                <div className={style.labelInput}>
+                                    <label htmlFor="addressName">Nombre</label>
+                                    <input 
+                                        type="text" 
+                                        id="addressName" 
+                                        name="name" 
+                                        value={address.name} 
+                                        onChange={handleAddressChange}
+                                    />
+                                </div>
+                                <div className={style.labelInput}>
+                                    <label htmlFor="addressStreet">Calle</label>
+                                    <input 
+                                        type="text" 
+                                        id="addressStreet" 
+                                        name="street" 
+                                        value={address.street} 
+                                        onChange={handleAddressChange}
+                                    />
+                                </div>
+                                <div className={style.variousInputs}>
+                                    <label htmlFor="addressNumber">Número</label>
+                                    <input 
+                                        type="text" 
+                                        id="addressNumber" 
+                                        name="number" 
+                                        value={address.number} 
+                                        onChange={handleAddressChange}
+                                    />
+                                    <label htmlFor="addressFloor">Piso</label>
+                                    <input 
+                                        type="text" 
+                                        id="addressFloor" 
+                                        name="floor" 
+                                        value={address.floor} 
+                                        onChange={handleAddressChange}
+                                    />
+                                    <label htmlFor="addressApartment">Departamento</label>
+                                    <input 
+                                        type="text" 
+                                        id="addressApartment" 
+                                        name="apartment" 
+                                        value={address.apartment} 
+                                        onChange={handleAddressChange}
+                                    />
+                                </div>
+                                <div className={style.labelInput}>
+                                    <label htmlFor="addressReference">Referencia</label>
+                                    <input 
+                                        type="text" 
+                                        id="addressReference" 
+                                        name="reference" 
+                                        value={address.reference} 
+                                        onChange={handleAddressChange}
+                                    />
+                                </div>
+                                <div className={style.addressButtons}>
+                                    <button type="button" onClick={addAddress}>
+                                        {editingAddressIndex !== null ? 'Actualizar' : 'Añadir'}
+                                    </button>
+                                    {editingAddressIndex !== null && (
+                                        <button type="button" onClick={cancelEdit} className={style.cancelButton}>
+                                            Cancelar
+                                        </button>
+                                    )}
+                                </div>
+                            </div>
+                                        
                             <div className={style.containerSubmit}>
                                 <button className={style.buttonSubmit} type="submit">Editar</button>
                             </div>
