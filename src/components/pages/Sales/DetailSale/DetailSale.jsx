4e6b10a02e204f9a53ddf920d7356690ba5dbb75
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-06-28 12:18:25 -0300

    debt info shows at sale detail

--- src/components/pages/Sales/DetailSale/DetailSale.jsx
+++ src/components/pages/Sales/DetailSale/DetailSale.jsx
@@ -357,6 +357,7 @@ const DetailSale = () => {
                                 {<p><span>Retención:&nbsp;</span> {saleDetail.paymentFee}% {`(- $${saleDetail.paymentFeeApplied})`}</p>}
                                 {saleDetail.totalWithFee && <p><span>Total con retención:&nbsp;</span> ${formatNumber(saleDetail.totalWithFee)}</p>}
                                 {saleDetail.totalPrice && <p><span>Total:&nbsp;</span> ${formatNumber(saleDetail.totalPrice)}</p>}
+                                {saleDetail.debt ? <p><span>Adeuda:&nbsp;</span> ${formatNumber(saleDetail.debt)}</p> : <></>}
                             </div>
                             <div className={style.column}>
                                 <p><span>Productos:&nbsp;</span></p>
